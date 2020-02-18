ea17cc59bac070057f6c3958c8a7bb5c472a2d13
Iván Aguilar <ivanalberto.agca@gmail.com>
2020-02-17 19:05:16 -0600

    Ejercicio de suma

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+
+    public static void main(String args[]) {
+
+        Integer numero1 = Integer.parseInt(args[0]);
+        Integer numero2 = Integer.parseInt(args[1]);
+
+        Integer resultado = numero1 + numero2;
+        
+        System.out.println("La suma es: " + resultado);
+    }
+
+}
