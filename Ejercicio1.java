1b3042e9ecff35e33443ee0bcd7196e5b6df2dea
Iván Aguilar <ivanalberto.agca@gmail.com>
2020-02-17 19:10:08 -0600

    Se agrego funcion suma

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,9 +5,13 @@ public class Ejercicio1 {
         Integer numero1 = Integer.parseInt(args[0]);
         Integer numero2 = Integer.parseInt(args[1]);
 
-        Integer resultado = numero1 + numero2;
-        
+        Integer resultado = hacerSuma(numero1, numero2);
+
         System.out.println("La suma es: " + resultado);
+    }
+
+    public static int hacerSuma(int numero1, int numero2) {
+        return numero1 + numero2;
     }
 
 }
