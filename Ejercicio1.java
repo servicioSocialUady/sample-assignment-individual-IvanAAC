7211367bf7127fa103618935606e903ffeb5b8ea
IvanAAC <61119352+IvanAAC@users.noreply.github.com>
2020-04-24 17:32:05 -0500

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,20 +1,46 @@
-public class Ejercicio1 {
+import java.util.Scanner;
+import java.math.BigInteger;
 
-    public static void main(String args[]) {
+public class Main {
 
-        Integer numero1 = Integer.parseInt(args[0]);
-        Integer numero2 = Integer.parseInt(args[1]);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                
+        Scanner in =new Scanner(System.in);
+        long entrada = in.nextLong();
+        
+        BigInteger numeroPrueba = new BigInteger(String.valueOf(entrada));
+                       
+        
+        int a = 0, b = 1, c = 0;
+        String resultado = "";        
+        boolean esPrimo = numeroPrueba.isProbablePrime(1);;
+        boolean esFibonacci = false;
+                        
+        while(c <= entrada){
+            c = a + b;		
+            a = b;
+            b = c;	
 
-        Integer resultado = hacerSuma(numero1, numero2);
+            if(c == entrada){
+                esFibonacci = true;
+            }            
+        }
 
-        System.out.println(resultado);
+        if(esFibonacci && esPrimo){
+            resultado = "asombroso";
+        }
+        else if(esFibonacci){
+            resultado = "fibonacci";
+        } 
+        else{
+            resultado = "ordinario";
+        }
+        
+        System.out.println(resultado);         
     }
-
-    public static int hacerSuma(int numero1, int numero2) {
-        int multi = 4 * 8;
-        int rest = 5 - 2;
-        int divi = 4/2;
-        return numero1 + numero2;
-    }
-
+    
 }
